002540d5e4f69860441d46dcbfdfa13e0f04e6f1
Крисеев Михаил <kriseev2000@yandex.ru>
2020-04-14 00:39:56 +0300

    getHomework.php + изменил проверку ошибок Теперь вывод метода проверяется перед преобразованием результата в объект

--- vkschoolbot-api-client-lib/BotAPI.cs
+++ vkschoolbot-api-client-lib/BotAPI.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
 namespace SchoolBotAPI
 {
     public class BotAPI
@@ -29,11 +32,61 @@ namespace SchoolBotAPI
         public async Task<HomeworkData> GetHomeworkAsync(int ID)
         {
             string url = $"{api_base_url}/getHomework.php?id={ID}&key={key}";
-            Console.WriteLine(url);
-            var result = JsonConvert.DeserializeObject<HomeworkData>(await GetRequestAsync(new Uri(url)));
-             
-            return result;
+            var request = GetRequestAsync(new Uri(url));
+            string successJsonSchema = @"{
+                'description': 'Homework data',
+                'type':'object',
+                'properties':
+                {
+                    'ID': {'type':'integer', 'required':true},
+                    'Subject':{'type':'string', 'required':true},
+                    'Homework':{'type':'string', 'required':true}
+                }
+            }";
+            Console.WriteLine("aa");
+            JSchema schema = JSchema.Parse(successJsonSchema);
+            Console.WriteLine("aa");
+            string json = (await request).Replace('"', '\'');
+            Console.WriteLine(json);
+            var result = JObject.Parse(json);
+            
+            var a = ValidateMethodOutput<HomeworkData>(schema, result);
+            return a;
+
         }
+
+        private T ValidateMethodOutput<T>(JSchema schema, JObject result)
+        {
+            Console.WriteLine(result.IsValid(schema));
+            if (result.IsValid(schema))
+            {
+                return result.ToObject<T>();
+            }
+            else
+            {
+                
+                string errorJsonSchema = @"{
+    'description':'Error',
+    'type':'object',
+    'properties':
+    {
+       'error':{'type':'string', 'required':true},
+       'errorcode':{'type':'integer', 'required':true}
+    }
+}";
+                JSchema errorSchema = JSchema.Parse(errorJsonSchema);
+                if (result.IsValid(errorSchema))
+                {
+                    handleError((string)result.GetValue("error"), (int)result.GetValue("errorcode"));
+                    return default(T);
+                }
+                else
+                {
+                    throw new BotAPIException("Не удалось обработать ответ сервера");
+                }
+            }
+        }
+
         public async Task AuthAsync(string username, string password)
         {
             var content = new FormUrlEncodedContent(new[]
@@ -42,42 +95,22 @@ namespace SchoolBotAPI
                 new KeyValuePair<string, string>("pass", password)
             });
 
-            string data = await PostRequestAsync("/auth.php", content);
-            
-            Dictionary<string, string> decodedData = JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
-            if (decodedData.ContainsKey("error"))
-            {
-                handleError(decodedData);
-            }
-            else if (decodedData.ContainsKey("key"))
-            {
-                key = decodedData["key"];
-            }
-            else
-            {
-                throw new BotAPIException("Не удалось распознать ответ сервера: " + data);
-            }
+            var request = PostRequestAsync("/auth.php", content);
+            string successJsonSchema = @"{
+                'description': 'Homework data',
+                'type':'object',
+                'properties':
+                {
+                    'key':{'type':'string', 'required':true}
+                }
+            }";
+
+            JSchema schema = JSchema.Parse(successJsonSchema);
+            JObject result = JObject.Parse(await request);
+            key = ValidateMethodOutput<KeyValuePair<string, string>>(schema, result).Value;
+
         }
 
-        private static void handleError(Dictionary<string, string> decodedData)
-        {
-            switch (decodedData["errorcode"])
-            {
-                case "1":
-                case "2":
-                    throw new ServerException(decodedData["error"]);
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                    throw new AuthenticationException(decodedData["error"]);
-                case "7":
-                    throw new MissingArgumentException(decodedData["error"]);
-                default:
-                    throw new BotAPIException("Произошла неизвестная ошибка");
-
-            }
-        }
         private static void handleError(string error, int errorcode)
         {
             switch (errorcode)
